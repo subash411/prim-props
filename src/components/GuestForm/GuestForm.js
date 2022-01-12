0c89626aaedf664db80bf86f8d0c17622f93f0c7
Subash Khatri <subashkhatri44@gmail.com>
2022-01-12 16:31:07 -0600

    organized code

---
+++ src/components/GuestForm/GuestForm.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+function GuestForm({
+    addGuest
+}) {
+    let [newGuestName, setNewGuestName] = useState('');
+    let [newGuestMeal, setNewGuestMeal] = useState('false');
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (newGuestName) {
+            addGuest({ name: newGuestName, kidsMeal: newGuestMeal });
+
+            setNewGuestName('');
+            setNewGuestMeal(false);
+        }
+        else {
+            alert('The new guest needs a name!');
+        }
+    }
+
+    return (
+        <>
+            <h2>Add a new guest</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name
+                </label>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={newGuestName}
+                    onChange={(evt) => setNewGuestName(evt.target.value)}
+                />
+                <div>
+                    Would this guest like a kid's meal?
+                        <div >
+                            <div>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value={true}
+                                        checked={newGuestMeal === 'true'}
+                                        name="kidsMeal"
+                                        onChange={(evt) => setNewGuestMeal(evt.target.value)}
+                                    />
+                                    Yes, this guest would like a Kid's Meal
+                                </label>
+                            </div>
+                            <div>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value={false}
+                                    checked={newGuestMeal === 'false'}
+                                    name="kidsMeal"
+                                    onChange={(evt) => setNewGuestMeal(evt.target.value)}
+                                />
+                                No, this guest would not like a Kid's Meal
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit">Add Guest</button>
+            </form>
+        </>
+    )
+}
+
+export default GuestForm
